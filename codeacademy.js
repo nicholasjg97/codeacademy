12eda5045becfb70577db66ed715a604e174866a
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-07-05 23:27:34 -0500

    working on refreshing basics

---
+++ codeacademy.js
@@ -0,0 +1,23 @@
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// 1. Use the += mathematical assignment operator to increase the value stored in levelUp by 5.
+// 2. Use the -= mathematical assignment operator to decrease the value stored in powerLevel by 100.
+// 3. Use the *= mathematical assignment operator to multiply the value stored in multiplyMe by 11.
+// 4. Use the /= mathematical assignment operator to divide the value stored in quarterMe by 4.
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+// 1. Using the increment operator, increase the value of gainedDollar.
+// 2. Using the decrement operator, decrease the value of lostDollar.
+
+gainedDollar++;
+lostDollar--;
