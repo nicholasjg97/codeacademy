b250febe579afb779852d16427a35e984c6c5e51
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-07-19 23:52:54 -0500

    finished previous problem

--- codeacademy.js
+++ codeacademy.js
@@ -50,19 +50,15 @@ console.log(typeof newVariable);
 
 // The forecast today is 293 Kelvin. To start, create a variable named kelvin, and set it equal to 293.
 // The value saved to kelvin will stay constant. Choose the variable type with this in mind.
-// Write a comment above that explains this line of code.
 // Celsius is similar to Kelvin — the only difference is that Celsius is 273 degrees less than Kelvin.
 // Let’s convert Kelvin to Celsius by subtracting 273 from the kelvin variable. Store the result in another variable, named celsius.
-// Write a comment above that explains this line of code.
 // Use this equation to calculate Fahrenheit, then store the answer in a variable named fahrenheit.
 
 // Fahrenheit = Celsius * (9/5) + 32
 
 // In the next step we will round the number saved to fahrenheit. Choose the variable type that allows you to change its value.
-// Write a comment above that explains this line of code.
 // When you convert from Celsius to Fahrenheit, you often get a decimal number.
 // Use the .floor() method from the built-in Math object to round down the Fahrenheit temperature. Save the result to the fahrenheit variable.
-// Write a comment above that explains this line of code.
 // Use console.log and string interpolation to log the temperature in fahrenheit to the console as follows:
 // The temperature is TEMPERATURE degrees Fahrenheit.
 // Use string interpolation to replace TEMPERATURE with the value saved to fahrenheit.
@@ -107,3 +103,12 @@ earlyYears = 2 * 10.5;
 let laterYears = myAge - 2;
 // taking two years from age for early years
 laterYears = laterYears * 4;
+// dogs years by your later years
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears = earlyYears + laterYears;
+// dog years my age
+var myName = 'Nicholas'.toLowerCase();
+// the built in method returns all lowercase
+console.log(myName);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
