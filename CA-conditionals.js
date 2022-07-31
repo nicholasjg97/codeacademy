12b92507f23fb5fb7d370b785f8e974a584abb56
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-07-30 22:00:41 -0500

    more review from codeacademy

--- CA-conditionals.js
+++ CA-conditionals.js
@@ -127,3 +127,46 @@ switch (athleteFinalPosition) {
   console.log('No medal awarded.');
   break;
 }
+
+
+
+// In this project we will build a Magic Eight Ball using control flow in JavaScript.
+// The user will be able to input a question, then our program will output a random fortune.
+
+let userName = 'Nico';
+if (userName) {
+  console.log(`Hello, ${userName}!`);
+} else {
+  console.log('Hello!');
+}
+const userQuestion = 'Will I become rich?';
+console.log(`${userName} asked, ${userQuestion}`);
+var randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+switch (randomNumber) {
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'It is decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  break;
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  default:
+  eightBall = 'Signs point to yes';
+  break;
+}
+console.log(eightBall);
