5e83da57c846f574d600a85c2c2616fa6021f2c6
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-08-09 23:49:36 -0500

    more review from codeacademy

---
+++ CA-loops.js
@@ -0,0 +1,22 @@
+// Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.
+// Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately.
+
+// Write your code below
+var vacationSpots = ['Houston', 'New York', 'Miami;']
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+
+// Create a program that loops from 5 to 10 and logs each number to the console.
+
+for (let counter = 5; counter < 11; counter++) {
+    console.log(counter);
+  }
+
+
+  Make a for loop that loops backwards printing 3 to 0 to the console. Use the >= comparison operator in your stopping condition and the -- operator in your iteration statement.
+
+  for (let counter = 3; counter >= 0; counter--) {
+    console.log(counter)
+  }
