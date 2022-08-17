995349721da444ccfe3adbbacb9f035d7af291fa
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-08-16 22:13:42 -0500

    more review from codeacademy

---
+++ CA-iterators.js
@@ -0,0 +1,26 @@
+// Iterate over the fruits array to log I want to eat a plus the name of each fruit to the console. For example, I want to eat a mango.
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+
+
+// Use .map() to create a new array that contains the first character of each string in the animals array. Save the new array to a const variable named secretMessage.
+// Use .map() to divide all the numbers in bigNumbers by 100. Save the returned values to a variable declared with const called smallNumbers.
+
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+const secretMessage = animals.map(letter => letter[0]);
+
+const bigNumbers = [100, 200, 300, 400, 500];
+const smallNumbers = bigNumbers.map(number => number / 100);
+
+
+// Call the .filter() method on randomNumbers to return values that are less than 250. Save them to a new array called smallNumbers, declared with const.
+// Invoke .filter() on the favoriteWords array to return elements that have more than 7 characters. Save the returned array to a const variable named longFavoriteWords.
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+const smallNumbers = randomNumbers.filter(num => num < 250);
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+const longFavoriteWords = favoriteWords.filter(word => word.length > 7);
+
+
