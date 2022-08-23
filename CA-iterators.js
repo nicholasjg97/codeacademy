14915ca722241f2dcab967d20422fe86904c9b8a
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-08-22 23:33:58 -0500

    more review from codeacademy

--- CA-iterators.js
+++ CA-iterators.js
@@ -53,3 +53,19 @@ console.log('The value of currentValue: ', currentValue);
 return accumulator + currentValue;
 console.log(newSum);
 }, 10)
+
+
+// The first thing we want to do is check if there are words that are fewer than 6 characters long. There is something missing in the words.some() method call. Fix this method so that true is printed to the console.
+// Use the .filter() method to save any words longer than 5 characters to a new variable called interestingWords, declared with const.
+// Complete the code between the parentheses to check if every element has more than 5 characters.
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+const interestingWords = words.filter(word => word.length > 5);
+
+console.log(interestingWords.every((word) => {
+  return word.length > 5} ));
