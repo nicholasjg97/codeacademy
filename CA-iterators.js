cb2956904c4dc1722906ffb264cb8a4b4db2c5d8
Nico Gaytan <nicholas.j.gaytan97@gmail.com>
2022-09-07 17:40:02 -0500

    review iterators from codeacademy

--- CA-iterators.js
+++ CA-iterators.js
@@ -69,3 +69,28 @@ const interestingWords = words.filter(word => word.length > 5);
 
 console.log(interestingWords.every((word) => {
   return word.length > 5} ));
+
+
+// Replace the word method in the first method call with a method that will do something to each of the values in the array and return undefined.
+// In the second method call, replace the word method with a method that will return a new array with only those elements longer than 7 characters.
+// In the third method call, replace the word method with a method that accepts an array containing multiple values and returns a single value.
+// In the fourth method call, replace the word method with a method that will return a new array of numbers returned from the function.
+// In the fifth method call, replace the word method with a method that will return a boolean value.
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+const longCities = cities.filter(city => city.length > 7);
+
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+const smallerNums = nums.map(num => num - 5);
+
+nums.every(num => num < 0);
